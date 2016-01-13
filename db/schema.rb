d8d0ff5cabf253c0a92aa73bb72b3046328b426f
darglk <darglk@gmail.com>
2016-01-13 23:19:47 +0100

    added avatar field to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113151226) do
+ActiveRecord::Schema.define(version: 20160113213801) do
 
   create_table "attachments", force: :cascade do |t|
     t.string   "file"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20160113151226) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "is_admin",               default: false
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
